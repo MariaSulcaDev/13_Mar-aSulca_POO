5fec76193366e7b6192bdcc4c330d26a3cb99118
MariaSulcaDev <maria.sulca.b@vallegrande.edu.pe>
2024-03-11 18:55:35 -0500

    Array 2

---
+++ 13_MariaSulca/src/S03_13/S3_13_array2.java
@@ -0,0 +1,22 @@
+package S03_13;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+
+/**
+ *
+ * @author Lenovo
+ */
+public class S3_13_array2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int[][] numeros2 = {{10, 20}, {30, 40}};
+        System.out.println(numeros2[1][0]);
+    }
+}
