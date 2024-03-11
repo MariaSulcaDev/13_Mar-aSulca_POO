87788b6be3bcd78623626a0da63b7af679cc89ce
MariaSulcaDev <maria.sulca.b@vallegrande.edu.pe>
2024-03-11 18:50:50 -0500

    Do-While

---
+++ 13_MariaSulca/src/S03_13/S3_13_DoWhile.java
@@ -0,0 +1,29 @@
+package S03_13;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+
+/**
+ *
+ * @author Lenovo
+ */
+public class S3_13_DoWhile {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+     //do = hacer
+     //while = mientras
+     int a = 100;
+     
+     do{
+         System.out.println("Hola");
+     } while (a == 200);
+    }
+    
+}
+
