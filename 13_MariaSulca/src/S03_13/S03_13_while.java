233a71bf5a3ceb92ca2920d08ddddbfad09fe660
MariaSulcaDev <maria.sulca.b@vallegrande.edu.pe>
2024-03-11 18:57:10 -0500

    While

---
+++ 13_MariaSulca/src/S03_13/S03_13_while.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package S03_13;
+
+/**
+ *
+ * @author Lenovo
+ */
+public class S03_13_while {
+
+    public static void main(String[] args) {
+        // TODO code application logic here
+
+        int a = 1;
+        while (a <= 10) {
+            System.out.println("Hola " + a);
+            a++;
+        }
+    }
+
+}
