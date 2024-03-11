aca806b090126dfc3640e6dba40a6a23afd83f8e
MariaSulcaDev <maria.sulca.b@vallegrande.edu.pe>
2024-03-11 18:54:18 -0500

    For

---
+++ 13_MariaSulca/src/S03_13/S3_13_For.java
@@ -0,0 +1,12 @@
+package S03_13;
+
+public class S3_13_For {
+
+    public static void main(String[] args) {
+      
+        for (int i = 1; i <= 10; i++){ 
+            System.out.println("Ejemplo numero " + i);
+        }
+    }
+    
+}
